b934f7d67f9471b18d0c4f1169253f39920480a3
duy9911 <44527223+duy9911@users.noreply.github.com>
2020-04-29 20:36:15 +0700

    Duy update
    29/4

---
+++ src/main/java/com/appdev/asm/config/WebSecurityConfig.java
@@ -0,0 +1,79 @@
+package com.appdev.asm.config;
+
+import javax.sql.DataSource;
+
+import com.appdev.asm.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+    private UserDetailsServiceImpl userDetailsService;
+ 
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
+    }
+ 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+ 
+        // Sét đặt dịch vụ để tìm kiếm User trong Database.
+        // Và sét đặt PasswordEncoder.
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+ 
+    }
+ 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+ 
+        http.csrf().disable();
+ 
+        // Các trang không yêu cầu login
+        http.authorizeRequests().antMatchers("/", "/login", "/logout","/home").permitAll();
+ 
+        // Trang /userInfo yêu cầu phải login với vai trò ROLE_USER hoặc ROLE_ADMIN.
+        // Nếu chưa login, nó sẽ redirect tới trang /login.
+        //http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+ 
+        // Trang chỉ dành cho ADMIN
+        http.authorizeRequests().antMatchers("/traineraccount-list","/traineraccount-save","/traineraccount-update","/trainingstaff-list","/trainingstaff-save","/trainingstaff-update").access("hasAuthority('admin')");
+        
+        http.authorizeRequests().antMatchers("/category-list","/category-save","/category-update","/course-list","/course-save","/course-update",
+        									"/topic-list","/topic-save","/topic-update","/trainee-list","/trainee-save","/trainee-update",
+        									"/traineecourse-list","/traineecourse-save","/trainer-list","/trainer-update","/trainertopic-list","/trainertopic-save").access("hasAuthority('trainingstaff')");
+        http.authorizeRequests().antMatchers("/viewcourse","/viewprofile","/trainerinfor-update").access("hasAuthority('trainer')");
+        // Khi người dùng đã login, với vai trò XX.
+        // Nhưng truy cập vào trang yêu cầu vai trò YY,
+        // Ngoại lệ AccessDeniedException sẽ ném ra.
+        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+ 
+        // Cấu hình cho Login Form.
+        http.authorizeRequests().and().formLogin()//
+                // Submit URL của trang login
+                .loginProcessingUrl("/j_spring_security_check") // Submit URL
+                .loginPage("/login")//
+                .defaultSuccessUrl("/home")//
+                .failureUrl("/login?error=true")//
+                .usernameParameter("username")//
+                .passwordParameter("password")
+                // Cấu hình cho Logout Page.
+                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/login?logout");
+
+ 
+    }
+ 
+}
